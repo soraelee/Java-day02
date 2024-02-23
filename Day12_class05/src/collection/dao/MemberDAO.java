721670fef0cf71956279a1e4486430919e35d3df
test123 <a@a.com>
2024-02-23 17:45:53 +0900

    Day 12일차Java 수강 완료

--- Day12_class05/src/collection/dao/MemberDAO.java
+++ Day12_class05/src/collection/dao/MemberDAO.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 
 import collection.dto.MemberDTO;
 
-public class MemberDAO { //Data Access Object : 저장소에 접근(연락) 
+public class MemberDAO { // Data Access Object : 저장소에 접근(연락)
 	public static ArrayList<MemberDTO> arr; // DTO를 ArrayList로 만들어줌
 	static {
-		arr = new ArrayList<>(); 
-		//static -> 없어지지 않고 계속 유지할 수 있어야 함
+		arr = new ArrayList<>();
+		// static -> 없어지지 않고 계속 유지할 수 있어야 함
 		// static으로 만들었기 때문에 static으로 초기화
-		//static{} 으로 사용하는 경우 => 코드를 여러줄 추가할 수 있음(초기 코드를 넣을 수 있음)
+		// static{} 으로 사용하는 경우 => 코드를 여러줄 추가할 수 있음(초기 코드를 넣을 수 있음)
 	}
-	
-	public void register(MemberDTO dto) { //들어온 값을 
+
+	public void register(MemberDTO dto) { // 들어온 값을
 		arr.add(dto); // 특정 저장소에 값을 저장하도록 연동
 	}
-	
-	public ArrayList<MemberDTO> getData() { //저장한 목록을 꺼낼 수 있도록 함
+
+	public ArrayList<MemberDTO> getData() { // 저장한 목록을 꺼낼 수 있도록 함
 		return arr;
-		
+
 	}
+
 	public MemberDTO search(String name) {
 		for (MemberDTO d : arr) {
 			if (name.equals(d.getName())) {
@@ -29,5 +30,5 @@ public class MemberDAO { //Data Access Object : 저장소에 접근(연락)
 		}
 		return null;
 	}
-	
+
 }
